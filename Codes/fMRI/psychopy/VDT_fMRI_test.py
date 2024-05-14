7cd1886980867fcdf89b3900305f74f0e87bca84
sepehr-mortaheb <sepehr.mortaheb@uantwerpen.be>
2024-05-14 14:16:59 +0200

    fMRI main block codes

--- Codes/fMRI/psychopy/VDT_fMRI_test.py
+++ Codes/fMRI/psychopy/VDT_fMRI_test.py
@@ -20,44 +20,102 @@ win = visual.Window(
      units='height'
 )
 timer = core.Clock()
-#log_file = 'trigger_times.log'
-#logging.LogFile(log_file, level=logging.INFO, filemode='w')
+log_file = 'trigger_times.log'
+logging.LogFile(log_file, level=logging.INFO, filemode='w')
 
-# Stimuli and other variables 
+# Stimuli and other variables
+tilt_degree = 2.5
 n_triggers = 5 # number of initial triggers from the scanner 
 
-    # Texts 
+    # Text Stimuli Variables 
 text_trigger = visual.TextStim(win, text="Waiting for the scanner triggers ...", color="white")
 text_trigger.size = (0.3, 0.1)
 
-text_tst = visual.TextStim(win, text="Hello!", color="white")
+    # Image Stimuli Variables 
+bg = visual.ImageStim(
+        win=win,
+        name='bg', 
+        image=f'./images_fMRI/grey_bg.png', 
+        size=(2, 1),
+        interpolate=True
+    )
+ref_white = visual.ImageStim(
+        win=win,
+        name='ref_white', 
+        image='./images_fMRI/Ref_white.png', 
+        #size=(1,1),
+        interpolate=True
+    )
+ref_yellow = visual.ImageStim(
+        win=win,
+        name='ref_yellow', 
+        image='./images_fMRI/Ref_yellow.png', 
+        #size=(1,1),
+        interpolate=True
+    )
+CW_white = visual.ImageStim(
+        win=win,
+        name='cw_white', 
+        image=f'./images_fMRI/Sphere_CW-{tilt_degree}_BG-grey_stim-white.png', 
+        #size=(1,1),
+        interpolate=True
+    )
+CW_yellow = visual.ImageStim(
+        win=win,
+        name='cw_yellow', 
+        image=f'./images_fMRI/Sphere_CW-{tilt_degree}_BG-grey_stim-yellow.png', 
+        #size=(1,1),
+        interpolate=True
+    )
+CCW_white = visual.ImageStim(
+        win=win,
+        name='ccw_white', 
+        image=f'./images_fMRI/Sphere_CCW-{tilt_degree}_BG-grey_stim-white.png', 
+        #size=(1,1),
+        interpolate=True
+    )
+CCW_yellow = visual.ImageStim(
+        win=win,
+        name='ccw_yellow', 
+        image=f'./images_fMRI/Sphere_CCW-{tilt_degree}_BG-grey_stim-yellow.png', 
+        #size=(1,1),
+        interpolate=True
+    )
+V_white = visual.ImageStim(
+        win=win,
+        name='v_white', 
+        image=f'./images_fMRI/Sphere_Ref_BG-grey_stim-white.png', 
+        #size=(1,1),
+        interpolate=True
+    )
+V_yellow = visual.ImageStim(
+        win=win,
+        name='v_yellow', 
+        image=f'./images_fMRI/Sphere_Ref_BG-grey_stim-yellow.png', 
+        #size=(1,1),
+        interpolate=True
+    )
 
-    # Images 
-#ref_white = visual.ImageStim(
- #       win=win,
-  #      name='ref_white', 
-   #     image='./images_fMRI/Ref_white.png', mask=None, anchor='center',
-    #    ori=0.0, pos=(0, 0), size=(2,1),
-     #   color=[1,1,1], colorSpace='rgb', opacity=None,
-      #  flipHoriz=False, flipVert=False,
-       # texRes=128.0, interpolate=True, depth=0.0)
-#ref_yellow = visual.ImageStim(
- #       win=win,
-  #      name='ref_yellow', 
-   #     image='./images_fMRI/Ref_yellow.png', mask=None, anchor='center',
-    #    ori=0.0, pos=(0, 0), size=(2,1),
-     #   color=[1,1,1], colorSpace='rgb', opacity=None,
-      #  flipHoriz=False, flipVert=False,
-       # texRes=128.0, interpolate=True, depth=0.0)
-#bg = visual.ImageStim(
- #       win=win,
-  #      name='bg', 
-   #     image=f'./images_fMRI/grey_bg.png', mask=None, anchor='center',
-    #    ori=0.0, pos=(0, 0), size=(2, 1),
-     #   color=[1,1,1], colorSpace='rgb', opacity=None,
-      #  flipHoriz=False, flipVert=False,
-       # texRes=128.0, interpolate=True, depth=0.0)
+stim_images = {
+    'Background': bg,
+    'WhiteRef': ref_white,
+    'YellowRef': ref_yellow,
+    'CWWhite': CW_white,
+    'CCWWhite': CCW_white,
+    'CWYellow': CW_yellow,
+    'CCWYellow': CCW_yellow,
+    'VWhite': V_white,
+    'VYellow': V_yellow
+}
 
+# Task Initialization 
+
+    # Block randomization 
+    ## It is better to give the order manually. If something happens during the 
+    ## the acquisition, it is not necessary to start from the beginning, as we 
+    ## we already now the order of the blocks. 
+block_order = ['M', 'C', 'C', 'M', 'M', 'C'] # M: main, C: control
+logging.log(msg="The block order is {}".format(block_order), level=logging.INFO)
 
 
 # Waiting for the scanner triggers 
@@ -68,22 +126,22 @@ trigger_times = wait_for_trigger(n_triggers, timer)
 
 # Task codes 
 ## Loop to present the text stimulus 10 times
-for _ in range(10):
-    # Display the text stimulus
-    text_tst.draw()
-    win.flip()
-    #t = timer.getTime()
-    #logging.log(msg="Stimulus white time: {}".format(t), level=logging.INFO)
-    print(timer.getTime())
-    
-    # Wait for 1 second
-    core.wait(1)
-    
-    # Clear the screen
-    win.flip()
-    
-    # Wait for 1 second (inter-stimulus interval)
-    core.wait(1)
+count = 1
+for block in block_order:
+    stim_order = stim_order_creator()
+    if block == 'M':
+        print(f"Running Block: {count} => Main")
+        ref_white.draw()
+        win.flip()
+        core.wait(2)
+        logging.log(msg="Start of the main block {} at {}".format(count, timer.getTime()), level=logging.INFO)
+        main_block(stim_order, stim_images, win, timer)
+        logging.log(msg="End of the main block {} at {}".format(count, timer.getTime()), level=logging.INFO)
+    #elif block == 'C':
+     #   logging.log(msg="Start of the control block {}: {}".format(count, timer.getTime()), level=logging.INFO)
+      #  control_block(stim_order, stim_images, win)
+    count += 1
+
 
 # Close the window
 win.close()
--- Codes/fMRI/psychopy/functions_fMRI.py
+++ Codes/fMRI/psychopy/functions_fMRI.py
@@ -1,5 +1,5 @@
 from psychopy import core, event, logging 
-
+import numpy as np
 
 def wait_for_trigger(n_triggers, timer):
     trigger_count = 0
@@ -14,9 +14,46 @@ def wait_for_trigger(n_triggers, timer):
         trigger_times.append(trigger_time)
     
         # Print trigger time
-        #logging.log(msg="Trigger {} time: {}".format(trigger_count + 1, trigger_time), level=logging.INFO)
+        logging.log(msg="Trigger {} time: {}".format(trigger_count + 1, trigger_time), level=logging.INFO)
         print("Trigger", trigger_count + 1, "time:", trigger_time)
     
         trigger_count += 1
 
     return trigger_times
+
+def stim_order_creator():
+    order = ['dev']*20 + ['std']*28
+    order = np.random.permutation(order)
+    out = []
+    for stim in order: 
+        if stim == 'dev':
+            out = out + ['dev', 'std', 'std']
+        elif stim == 'std':
+            out = out + ['std']
+    out = ['std', 'std'] + out
+
+    return out
+
+def main_block(stim_order, stim_images, win, timer):
+    std_idx = np.where(np.array(stim_order) == 'std')[0]
+    np.random.shuffle(std_idx)
+    for idx in std_idx[0:35]:
+        stim_order[idx] = 'std_cw'
+    for idx in std_idx[35:70]:
+        stim_order[idx] = 'std_ccw'
+
+    for stim in stim_order:
+        if stim == 'dev':
+            image = stim_images['VWhite']
+        elif stim == 'std_cw':
+            image = stim_images['CWWhite']
+        elif stim == 'std_ccw':
+            image = stim_images['CCWWhite']
+
+        logging.log(msg="Trial {} at {}".format(stim, timer.getTime()), level=logging.INFO)
+        image.draw()
+        win.flip()
+        core.wait(1)
+        win.flip()
+        core.wait(1)
+    
